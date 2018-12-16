87d1df0072ff1dd58957ea42f87c63cb9fd62c2f
dannykrug <dannykrug@gmail.com>
2018-12-16 16:36:58 -0500

    added all routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,66 @@
 import React, {Component} from 'react'
 import {NavLink} from 'react-router-dom'
+import './NavBar.css'
+
+const menuitem = {
+ listStyle: 'none',
+ textDecoration: 'none',
+ fontFamily: 'Abel',
+ color: '#289bff',
+ fontSize: '24px',
+ fontWeight: 'bold',
+ padding: '0 20px 0 20px'
+}
 
 class NavBar extends Component{
   render() {
     return(
       <div>
+      <div>
+      <NavLink
+        to='/'
+        exact
+        style={menuitem}
+        activeStyle={{
+          color: 'black'
+        }}
+        >Home</NavLink>
+        <NavLink
+          to='/projects'
+          exact
+          style={menuitem}
+          activeStyle={{
+            color: 'black'
+          }}
+          >Projects</NavLink>
+          <NavLink
+            to='/about'
+            exact
+            style={menuitem}
+            activeStyle={{
+              color: 'black'
+            }}
+            >About</NavLink>
+            <NavLink
+              to='/blogs'
+              exact
+              style={menuitem}
+              activeStyle={{
+                color: 'black'
+              }}
+              >Blogs</NavLink>
+              <NavLink
+                to='/contact'
+                exact
+                style={menuitem}
+                activeStyle={{
+                  color: 'black'
+                }}
+                >Contact</NavLink>
+      </div>
       </div>
     )
   }
 }
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -5,13 +5,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router, Route, NavLink, withRouter} from 'react-router-dom'
 import NavBar from '../src/components/NavBar'
+import About from '../src/components/About'
+import Blogs from '../src/components/Blogs'
+import Projects from '../src/components/Projects'
+import Contact from '../src/components/Contact'
 
 function renderSite  () {
   return(
     <div>
     <Router>
     <div>
+    <NavBar/>
     <Route exact path='/' component={App}/>
+    <Route exact path='/about' component={About}/>
+    <Route exact path='/projects' component={Projects}/>
+    <Route exact path='/contact' component={Contact}/>
+    <Route exact path='/blogs' component={Blogs}/>
     </div>
     </Router>
     </div>
